0341d786e0553083e9f98ced6b6909ee9dc22daa
ghtp25 <ghtp25@gmail.com>
2015-01-25 17:49:58 -0200

    update

---
+++ 4-dialog/dialog-backtitle.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dialog --backtitle "Logicus Tecnologia de Informação e Comunicação" \
+	--title "CAL Central Administradora de Linux 0.1" \
+	--msgbox "Olá. Este programa é de uso exclusivo do \
+	administrador de sistemas. Suas funções buscam uma \
+	organização centralizada." 0 0
+
---
+++ 4-dialog/dialog-msgbox.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dialog --title "Exemplo de msgbox" \
+	--backtitle "Aprendendo shell" \
+	--msgbox "Aqui está a mensagem de exemplo" 0 0
