10eec1cf23964ab47f7a2deda43b9dfbbff38d09
ghtp25 <ghtp25@gmail.com>
2015-02-06 04:42:18 -0200

    update

--- 10-instaladores/instala-puppet-debian-wheezy.sh
+++ 10-instaladores/instala-puppet-debian-wheezy.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
+# Auxilia na instalação do Puppet
+# no Debian Wheezy a partir do
+# repositório da Puppet Labs
+
+# Verificando se usuário tem permissão de instalação
+if [[ "$UID" != "0" ]]; then
+	echo "Você precisa ser o root para instalar o Puppet"
+	exit 0
+fi
+
+# Dirigindo-se para o diretório do root
 cd ~
+
+# Baixando o release da Puppet Labs, com ele
+# você terá acesso a pacotes mais atuais do Puppet
 wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
+
+# Instalando o release
 dpkg -i puppetlabs-release-wheezy.deb
+
+# Atualizando repositório
 apt-get update
-apt-get install puppet
+
+# Instalando o Puppet
+apt-get install puppet -y
+
+# Limpando lixo da instalação
 rm puppetlabs*
+
 echo "Concluído"
