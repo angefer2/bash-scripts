d13917975a1d437309b1de6f6a6c991bae186797
gustavo <gustavo@gustavo>
2015-02-09 13:07:51 -0200

    update

---
+++ 3-avancados/firewall.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Nome: firewall.sh
+# Descrição: script simples de firewall
+# Autor: Gustavo S. de Lima
+# Email: gustavo@logicus.com.br
+
+# Instruções no Debian
+# chmod 755 /etc/init.d/firewall.sh
+# update-rc.d defaults firewall.sh
+
+
+IPTABLES="/sbin/iptables"
+
+IFACE_LO=""
+IP_IFACE_LO=""
+
+IFACE_EXT=""
+IP_IFACE_EXT=""
+IP_REDE_EXT=""
+IP_BROADCAST_EXT=""
+MASC_REDE_EXT=""
+
+IFACE_INT=""
+IP_IFACE_INT=""
+IP_REDE_INT=""
+IP_BROADCAST_INT=""
+MASC_REDE_INT=""
+
+case "$1" in
+
+	start)
+	echo "Iniciando Firewall..."
+	$IPTABLES -F
+	$IPTABLES -F -t nat
+	$IPTABLES -F -t mangle
+	$IPTABLES -X
+
+	# Políticas padrão
+	$IPTABLES -P INPUT DROP
+	$IPTABLES -P FORWARD DROP
+	$IPTABLES -P OUTPUT DROP
+	;;
+
+	stop)
+	echo "Parando Firewall..."
+	$IPTABLES -F
+	$IPTABLES -F -t nat
+	$IPTABLES -F -t mangle
+	;;
+
+	restart)
+	$0 stop
+	$0 start
+	;;
+
+	status)
+	$IPTABLES -L
+	$IPTABLES -L -t nat
+	$IPTABLES -L -t mangle
+	;;
+
+	*)
+	echo "Use $0 {start|stop|restart|status}"
+	;;
+
+esac
+
+exit 0
