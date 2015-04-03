146484a0c3b256d673965d4530ffdd454ca482aa
ghtp25 <ghtp25@gmail.com>
2015-04-03 18:24:42 -0300

    update

--- 10-instaladores/instala-seguranca-basica-debian.sh
+++ 10-instaladores/instala-seguranca-basica-debian.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 # Descrição: ações para segurança mínima
+# Sistema: Debian
 
-apt-get install -y portsentry
-apt-get install -y lynis
+# Verificando se usuário tem permissão de instalação
+if [[ "$UID" != "0" ]]; then
+	echo "Você precisa possuir privilégios de super usuário!"
+	echo "Tente novamente como root ou utilize sudo se possuir."
+        exit 0
+    else
+    echo "Iniciando instalação de programas úteis para segurança..."
+    sleep 2
+fi
+
+# Usando o for para listar os 
+# programas a serem instalados
+for programa in clamav denyhosts iptables lynis portsentry tripwire
+
+do
+	echo "Instalando $programa ..."
+	apt-get install -y $programa
+done
+
+echo "Script concluído!"
