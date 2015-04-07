be1832e9d8c87d480d93a88ad3e71335c68baafe
ghtp25 <ghtp25@gmail.com>
2015-04-06 21:47:44 -0300

    update

---
+++ 1-basicos/while/while-ao-cubo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+n=0
+while (( n < 10 ))
+do
+
+	(( cubo = n * n * n ))
+	echo "O número $n ao cubo é $cubo!"
+	(( n = n + 1 ))
+done
--- 1-basicos/while/while-read-tabuada.sh
+++ 1-basicos/while/while-read-tabuada.sh
@@ -6,10 +6,11 @@
 	do
 	valor1=${RANDOM:1:1}
 	valor2=${RANDOM:1:1}
+	echo "Você tem 5 segundos para responder..."
 	echo "Quanto é $valor1 * $valor2?"
 	read -t 5
 	echo "O resultado seria $[valor1*valor2]"
-	sleep 5
+	sleep 2
 	done
 
 # Neste script podemos ver:
