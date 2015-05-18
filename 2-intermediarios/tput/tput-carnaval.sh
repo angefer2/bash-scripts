0e7914ba22db0aab307123e83f6d738ae7be3f5c
ghtp25 <ghtp25@gmail.com>
2015-05-17 22:11:02 -0300

    update

--- 2-intermediarios/tput/tput-carnaval.sh
+++ 2-intermediarios/tput/tput-carnaval.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# O primeiro for é para a cor da letra
 for corletra in 1 2 3 4 5 6 7 8;
 do
+	# este segundo for determina as cores de fundo
 	for corfundo in 1 2 3 4 5 6 7 8;
 		do
 		tput setaf $corletra; 
 		tput setab $corfundo; 
 		echo -ne " ** Carnaval Logicus 2015, a gente vai se vê no Shell ** "; 	
-		tput sgr0; 
+		tput sgr0;
 	done; 
 done
